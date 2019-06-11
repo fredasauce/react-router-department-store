a6e1f4eb48c7c87cdc4f074fd1419975acbe3e90
Alfreda Tsai <alfredatsai@gmail.com>
2019-06-11 15:27:41 -0600

    all controllers built and seeded

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -1,16 +1,47 @@
 class Api::ItemsController < ApplicationController
+  before_action :set_department
+  before_action :set_item, only: [:show, :update, :destroy]
+
   def index
+    render json: @department.items.all
   end
 
   def show
+    render json: @department.item
   end
 
   def create
+    item = @department.items.new(item_params)
+
+    if item.save
+      render json: item
+    else
+      render json: item.errors, status:422
+    end
   end
 
   def update
+    if @item.update(item_params)
+      render json: @item
+    else
+      render json: @item.errors, status: 422
   end
 
   def destroy
+    @item.destroy
   end
+
+  private
+
+    def set_department
+      @department = Department.find(params[:department_id])
+    end
+
+    def set_item
+      @item = Item.find(params[:id])
+    end
+
+    def item_params
+      params.require(:item).permit(:name, :description, :price)
+    end
 end
