0b6eb9ccb293c556b787090f8c66eebb38b1af2e
Alfreda Tsai <alfredatsai@gmail.com>
2019-06-11 16:11:15 -0600

    home, about, nomatch, and departments pages made

---
+++ client/src/components/Home.js
@@ -0,0 +1,8 @@
+import React from "react"
+import { Header, } from "semantic-ui-react"
+
+const Home = () => (
+  <Header as="h1" style={{ padding: "25px" }}>Home</Header>
+);
+
+export default Home;
---
+++ client/src/components/NoMatch.js
@@ -0,0 +1,8 @@
+import React from "react"
+import { Header, } from "semantic-ui-react"
+
+const NoMatch = () => (
+  <Header as="h1" style={{ padding: "25px" }}>404 error: Page Not Found</Header>
+);
+
+export default NoMatch;
